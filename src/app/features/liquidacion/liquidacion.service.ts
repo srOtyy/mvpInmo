d896d84a04e9031343d150599eec6334298306b7
srOtyy <oty2013@outlook.com>
2026-05-21 15:38:28 -0300

    Se genera un .docx

--- src/app/features/liquidacion/liquidacion.service.ts
+++ src/app/features/liquidacion/liquidacion.service.ts
@@ -3,21 +3,24 @@ import { Injectable } from '@angular/core';
 import PizZip from 'pizzip';
 import Docxtemplater from 'docxtemplater';
 import { saveAs } from 'file-saver';
-import { Liquidacion, LiquidacionItem } from './liquidacion-interface';
+import { Liquidacion } from './liquidacion-interface';
 import { IContrato } from '../contrato/contrato.interface';
 import { randomId } from '../../shared/utilitys';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class LiquidacionGeneratorService {
-  constructor() { }
+  constructor( private _http: HttpClient ) { }
+
   crearLiquidacion(contrato: IContrato, nombrePropietario: string, nombreInquilino: string): Liquidacion {
     const liquidacion: Liquidacion = {
       id: randomId().toString() ,
       contratoId: contrato.id.toString(),
       propietarioNombre: nombrePropietario,
       inquilinoNombre: nombreInquilino,
-      periodo: `${contrato.fechaInicio} - ${contrato.fechaFin}`,
+      periodo: `Inicio: ${contrato.fechaInicio} - Fin: ${contrato.fechaFin}`,
       fechaGeneracion: new Date(),
       items: [],
       total: contrato.rentaMensual
@@ -26,32 +29,37 @@ export class LiquidacionGeneratorService {
     return liquidacion
   }
   async generar(liquidacion: Liquidacion): Promise<void> {
+    try{
+      const response = await lastValueFrom(
+        this._http.get('/templates/liquidacion-base.docx', { responseType: 'arraybuffer' })
+      );
+      const content = new Uint8Array(response as ArrayBuffer);
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true
+      });
 
-    const response = await fetch(
-      '../../templates/liquidacion-base.docx'
-    );
-    const content = await response.arrayBuffer();
-    const zip = new PizZip(content);
-    const doc = new Docxtemplater(zip, {
-      paragraphLoop: true,
-      linebreaks: true
-    });
+      doc.render({
+        contrato: liquidacion.contratoId,
+        periodo: liquidacion.periodo,
+        propietario: liquidacion.propietarioNombre,
+        inquilino: liquidacion.inquilinoNombre,
+        items: liquidacion.items,
+        total: liquidacion.total
+      });
 
-    doc.render({
-      contratoNumero: liquidacion.contratoId,
-      periodo: liquidacion.periodo,
-      propietario: liquidacion.propietarioNombre,
-      inquilino: liquidacion.inquilinoNombre,
-      items: liquidacion.items,
-      total: liquidacion.total
-    });
+      const blob = doc.getZip().generate({
+        type: 'blob',
+        mimeType:
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      });
+      console.log("liquidacion generada:", liquidacion);
+      saveAs(blob, 'liquidacion.docx');
 
-    const blob = doc.getZip().generate({
-      type: 'blob',
-      mimeType:
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-    });
-
-    saveAs(blob, 'liquidacion.docx');
+    }
+    catch(error){
+      console.error('Error al generar la liquidación;', error);
+    }
   }
 }
