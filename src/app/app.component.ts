0d09b8cd62238de56e52fa8f9f128de74317e215
srOtyy <oty2013@outlook.com>
2026-06-01 17:29:41 -0300

    pre-lista contratos (sidebar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { MainComponent } from './layout/main/main.component';
 import { HeadComponent } from './layout/head/head.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { A11yModule } from "@angular/cdk/a11y";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent,MainComponent,HeadComponent],
+  imports: [SidebarComponent, MainComponent, HeadComponent, MatButtonModule, MatSidenavModule, A11yModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  abrirSidenav: boolean = false;
   // el dia que empece fue el 1/12 ?
+   eventoSidenav(){
+    this.abrirSidenav = !this.abrirSidenav;
+  }
+
 }
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { RutasDinamicasService } from '../../core/navegacion/rutas-dinamicas.service';
 import { Dominio, IBotonRuta } from '../../core/navegacion/navegacionRutas';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  evento = output<void>();
   // no se si estoy agarrando el valor del signal y lo asigno a la variable o si directamente uso el signal en el html, lo que si se es que el signal no se puede usar en el html, por eso lo asigno a la variable dominioActivo, pero no se si es la mejor forma de hacerlo
   dominiosActivos: string[] = [];
   dominioActivo: Dominio | null = null;
@@ -22,7 +24,9 @@ export class SidebarComponent {
     });
     
   }
-
+  cerrarSidenavEvent() {
+    this.evento.emit();
+  }
   enviarDominio(dominio: string): void {
     this.rutasDinamicasService.$dominioActivo.set(dominio as Dominio);
   }
