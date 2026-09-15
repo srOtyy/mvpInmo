dd3cc9065c0f005729ae380eff6d6ab112109943
srOtyy <oty2013@outlook.com>
2026-09-15 11:17:07 -0300

    chau headd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { MainComponent } from './layout/main/main.component';
-import { HeadComponent } from './layout/head/head.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { A11yModule } from "@angular/cdk/a11y";
+import { A11yModule } from '@angular/cdk/a11y';
 import { VistaInfoEntidadComponent } from './shared/vista-info-entidad/vista-info-entidad.component';
 import { ContratoBbddService } from './features/contrato/contrato-bbdd.service';
 
 @Component({
-    selector: 'app-root',
-    imports: [SidebarComponent, MainComponent, HeadComponent, MatButtonModule, MatSidenavModule, A11yModule, VistaInfoEntidadComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [
+    SidebarComponent,
+    MainComponent,
+    MatButtonModule,
+    MatSidenavModule,
+    A11yModule,
+    VistaInfoEntidadComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   abrirSidenav: boolean = false;
-  constructor(public _contratos: ContratoBbddService){}
+  constructor(public _contratos: ContratoBbddService) {}
   // el dia que empece fue el 1/12 ?
-   eventoSidenav(){
+  eventoSidenav() {
     this.abrirSidenav = !this.abrirSidenav;
   }
- 
 }
